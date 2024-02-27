981b2c1a412f53dd985cc3fc8a7e98e684f88362
MadBowJam <madbowjam@gmail.com>
2024-02-27 21:04:55 +0200

    0.14 alpha | hello2 ?

--- assets/js/index.js
+++ assets/js/index.js
@@ -89,6 +89,9 @@ fetch('./assets/json/prices/27.02.2024__20_09.json')
 
 function myFunction() {
   for (let i = 1; i < itemArray.length; i++) {
+    console.log(document.getElementById(`price${i}`).innerHTML);
+    console.log(document.getElementById(`quantity${i}`).innerHTML);
+    // console.log()
     document.getElementById(`total${i}`).innerHTML = Number(document.getElementById(`price${i}`).innerHTML) * Number(document.getElementById(`quantity${i}`).innerHTML)
   }
 }
